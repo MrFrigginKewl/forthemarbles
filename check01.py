d870398255b9416adbd64258615db384ca23e3d9
student <j4c0bv123@gmail.com>
2023-09-01 13:18:09 -0500

    When 2

---
+++ check01.py
@@ -0,0 +1,10 @@
+numberinput = input("Please type a number between 1 and 100.")
+import random
+therandom=random.randint(1,100)
+
+print('You chose', numberinput, 'and the number', therandom ,'was generated.')
+print(f'You chose {numberinput} and the number {therandom} was generated.')
+
+if numberinput == therandom:
+	print('The numbers are the same.'
+elseif 
---
+++ popquiz.py
@@ -0,0 +1,6 @@
+for number in range(1,100):
+	if (number % 5 == 0):	
+		print('Hit!!!')
+	else:
+		print(number)
+# forthemarbles
